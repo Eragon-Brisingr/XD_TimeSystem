9bdf55556d37ea7bb672954c7a3be9f58e1006ea
Eragon-Brisingr <450614754@qq.com>
2018-09-11 23:09:32 +0800

    处理Editor依赖

--- Source/XD_TimeSystem/Private/XD_TimeManagerPreview.cpp
+++ Source/XD_TimeSystem/Private/XD_TimeManagerPreview.cpp
@@ -3,8 +3,6 @@
 #include "XD_TimeManagerPreview.h"
 #include "XD_TimeManager.h"
 
-#if WITH_EDITOR
-
 AXD_TimeManagerPreviewActor::AXD_TimeManagerPreviewActor()
 {
 	PrimaryActorTick.bCanEverTick = true;
@@ -14,10 +12,12 @@ AXD_TimeManagerPreviewActor::AXD_TimeManagerPreviewActor()
 	PreviewTimeManager = CreateDefaultSubobject<UXD_TimeManager>(GET_MEMBER_NAME_CHECKED(AXD_TimeManagerPreviewActor, PreviewTimeManager));
 }
 
+#if WITH_EDITOR
 void AXD_TimeManagerPreviewActor::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
 {
 	Super::PostEditChangeProperty(PropertyChangedEvent);
 }
+#endif // WITH_EDITOR
 
 void AXD_TimeManagerPreviewActor::TickActor(float DeltaSeconds, ELevelTick TickType, FActorTickFunction& ThisTickFunction)
 {
@@ -44,4 +44,3 @@ void AXD_TimeManagerPreviewActor::OnConstruction(const FTransform& Transform)
 	}
 }
 
-#endif // WITH_EDITOR
